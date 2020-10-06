da680c9aaa5dee1af328295e22664bafe869e814
yueyang2000 <2018011359@secoder.net>
2020-10-07 00:02:57 +0800

    try to build proxy

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,2 +1,3 @@
-export const API_BASE_URL = process.env.REACT_APP_SERVER_URL;
+//export const API_BASE_URL = process.env.REACT_APP_SERVER_URL;
+export const API_BASE_URL = '';
 export const ACCESS_TOKEN_NAME = 'login_access_token';
--- src/utils/communication.js
+++ src/utils/communication.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
-import {API_BASE_URL} from './api';
+//import {API_BASE_URL} from './api';
 
 export function register(payload){
-    return axios.post(API_BASE_URL+'/user/register', payload);
+    return axios.post('user/register', payload);
 }
 
 
 export function login(payload){
-    return axios.post(API_BASE_URL+'/user/login', payload);
+    return axios.post('user/login', payload);
 }
