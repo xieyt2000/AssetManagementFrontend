a42adcab56b977790a45f550c053d9efbe575c37
Xie Yuntong <2018011334@secoder.net>
2020-10-11 01:27:00 +0800

    HelpCard component. Ref#4

---
+++ src/components/HelpCard/index.jsx
@@ -0,0 +1,23 @@
+import { Card } from 'antd'
+import React from 'react'
+import { PropTypes } from 'prop-types'
+
+const HelpCard = (props) => {
+  return (
+    <Card bordered={false} className='card-item' title={props.title}>
+      <p dangerouslySetInnerHTML={{ __html: this.source }}/>
+    </Card>
+  )
+}
+
+HelpCard.propTypes = {
+  title: PropTypes.string,
+  source: PropTypes.string
+}
+
+HelpCard.defaultProps = {
+  title: '',
+  source: ''
+}
+
+export default HelpCard
