3e0aeb11be084a6166d40f3f4571b96dc8c77e89
黄翘楚 <2018011363@secoder.net>
2020-10-10 22:49:54 +0800

    添加content组件，并实现User路由 Ref #6

---
+++ src/views/layout/Content/index.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import { Redirect, withRouter, Route, Switch } from 'react-router-dom'
+import DocumentTitle from 'react-document-title'
+import { connect } from 'react-redux'
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
+import { Layout } from 'antd'
+import { getMenuItemInMenuListByProperty } from '@/utils'
+import routeList from '@/config/routeMap'
+import menuList from '@/config/menuConfig'
+import PropTypes from 'prop-types'
+
+const { Content } = Layout
+
+const getPageTitle = (menuList, pathname) => {
+  let title = 'Ant Design Pro'
+  const item = getMenuItemInMenuListByProperty(menuList, 'path', pathname)
+  if (item) {
+    title = `${item.title}`
+  }
+  return title
+}
+
+const LayoutContent = (props) => {
+  const { role, location } = props
+  const { pathname } = location
+  const handleFilter = (route) => {
+    // 过滤没有权限的页面
+    return role === 'admin' || !route.roles || route.roles.includes(role)
+  }
+  return (
+    <DocumentTitle title={getPageTitle(menuList, pathname)}>
+      <Content style={{ height: 'calc(100% - 100px)' }}>
+        <TransitionGroup>
+          <CSSTransition
+            key={location.pathname}
+            timeout={500}
+            classNames="fade"
+            exit={false}
+          >
+            <Switch location={location}>
+              <Redirect exact from="/" to="/dashboard"/>
+              {routeList.map((route) => {
+                return (
+                  handleFilter(route) && (
+                    <Route
+                      component={route.component}
+                      key={route.path}
+                      path={route.path}
+                    />
+                  )
+                )
+              })}
+              <Redirect to="/error/404"/>
+            </Switch>
+          </CSSTransition>
+        </TransitionGroup>
+      </Content>
+    </DocumentTitle>
+  )
+}
+LayoutContent.propTypes = {
+  role: PropTypes.string,
+  location: PropTypes.instanceOf(window.location),
+  pathname: PropTypes.string
+}
+
+export default connect((state) => state.user)(withRouter(LayoutContent))
--- src/views/layout/index.jsx
+++ src/views/layout/index.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import { connect } from 'react-redux'
-// import Content from './Content'
+import Content from './Content'
 import Header from './Header'
 // import RightPanel from './RightPanel'
 import Sider from './Sider'
 import { Layout } from 'antd'
+
 const Main = (props) => {
   return (
     <Layout style={{ minHeight: '100vh' }}>
-      <Sider />
+      <Sider/>
       <Layout>
-        <Header />
-        {/* <Content /> */}
+        <Header/>
+        <Content/>
         {/* <RightPanel /> */}
       </Layout>
     </Layout>
