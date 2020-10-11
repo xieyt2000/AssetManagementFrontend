a0dc285677e32aba9f9fd49514d59315ba83183a
Xie Yuntong <2018011334@secoder.net>
2020-10-11 22:54:57 +0800

    change mock. Ref#8

--- src/mock/index.js
+++ src/mock/index.js
@@ -2,8 +2,8 @@ import Mock from 'mockjs'
 import loginAPI from './login'
 
 // 登录与用户相关
-Mock.mock(/\/login/, 'post', loginAPI.login)
-Mock.mock(/\/logout/, 'post', loginAPI.logout)
-Mock.mock(/\/userInfo/, 'post', loginAPI.userInfo)
+Mock.mock(/\/user\/login/, 'post', loginAPI.login)
+Mock.mock(/\/user\/logout/, 'post', loginAPI.logout)
+Mock.mock(/\/user\/info/, 'post', loginAPI.userInfo)
 
 export default Mock
