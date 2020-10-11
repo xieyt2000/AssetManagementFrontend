047a408d4116a5dd6d86c7b48dc29bcf6ec75a82
Xie Yuntong <2018011334@secoder.net>
2020-10-11 22:01:19 +0800

    Edit permission constants for role list. Ref#4

--- src/utils/permission.js
+++ src/utils/permission.js
@@ -1,9 +1,17 @@
 const PERMISSION = {
-  IT: 'it',
-  ASSET: 'asset',
-  SYSTEM: 'system',
-  STAFF: 'staff'
+  IT: 'IT',
+  ASSET: 'ASSET',
+  SYSTEM: 'SYSTEM',
+  STAFF: 'STAFF'
 }
+
+export const CHINESE_PERMISSION = {
+  IT: 'IT管理员',
+  ASSET: '资产管理员',
+  SYSTEM: '系统管理员',
+  STAFF: '员工'
+}
+
 export const checkPermission = (roles, role) => {
   for (let i = 0; i < role.length; i++) {
     if (roles.includes(role[i])) {
