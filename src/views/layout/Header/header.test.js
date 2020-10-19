090978d66b09b372e704cc85367642d7dc0f846a
yueyang2000 <2018011359@secoder.net>
2020-10-19 21:55:37 +0800

    add render test for multiple views, fix proptypes bug. Ref #12.

--- src/views/layout/Header/header.test.js
+++ src/views/layout/Header/header.test.js
@@ -1,20 +1,20 @@
 import React from 'react'
 import { Provider } from 'react-redux'
 import renderer from 'react-test-renderer'
-import BreadCrumb from './index'
 import { HashRouter } from 'react-router-dom'
 import { store } from '@/utils/mockStore'
-describe('BreadCrumb test', () => {
+import LayoutHeader from './index'
+describe('LayoutHeader test', () => {
   let component
   beforeEach(() => {
     component = renderer.create(
       <Provider store={store}>
         <HashRouter>
-          <BreadCrumb />
+          <LayoutHeader />
         </HashRouter>
       </Provider>
     )
   })
-  it('render BreadCrumb', () => {
+  it('render', () => {
   })
 })
--- src/views/user-management/user-management.test.js
+++ src/views/user-management/user-management.test.js
@@ -1,20 +1,17 @@
 import React from 'react'
 import { Provider } from 'react-redux'
 import renderer from 'react-test-renderer'
-import BreadCrumb from './index'
-import { HashRouter } from 'react-router-dom'
 import { store } from '@/utils/mockStore'
-describe('BreadCrumb test', () => {
+import UserManagement from './index'
+describe('user management test', () => {
   let component
   beforeEach(() => {
     component = renderer.create(
       <Provider store={store}>
-        <HashRouter>
-          <BreadCrumb />
-        </HashRouter>
+        <UserManagement />
       </Provider>
     )
   })
-  it('render BreadCrumb', () => {
+  it('render', () => {
   })
 })
