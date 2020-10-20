e0a2fb26b3a9b581e7330b34f9c23456c8eaafa8
yueyang2000 <2018011359@secoder.net>
2020-10-20 21:42:28 +0800

    remove expand icon in asset table. Ref #15

--- src/views/asset-management/index.jsx
+++ src/views/asset-management/index.jsx
@@ -60,7 +60,7 @@ class AssetManagement extends Component {
         <Button type='primary' onClick={this.handleAdd}>导入资产</Button>
         <Divider type='vertical'/>
         <UploadAsset/>
-
+        <Divider type='vertical'/>
       </span>
     )
     const description = '作为资产管理员，你可以进行资产管理和批量导入、导出'
@@ -71,12 +71,13 @@ class AssetManagement extends Component {
           <Table
             bordered rowKey="name"
             dataSource={assetList}
-            pagination={true}>
+            expandIconColumnIndex={-1}
+            pagination={false}>
             <Column title="资产名称" dataIndex="name" key="name" align="center"/>
             <Column title="挂账人" dataIndex="owner" key="owner" align="center"/>
             <Column title="所属部门" dataIndex="department" key="department" align="center"/>
-            <Column title="资产类型" dataIndex="is_quantity" key="is_quantity" align="center" render={(row) => (
-              <span> {row.is_quantity ? '数量型' : '条目型'} </span>
+            <Column title="资产类型" dataIndex="isQuantity" key="isQuantity" align="center" render={(row) => (
+              <span> {row.isQuantity ? '数量型' : '条目型'} </span>
             )}/>
             <Column title="资产价值" dataIndex="value" key="value" align="center"/>
             <Column title="操作" key="action" width={200} align="center" render={(row) => (
