2852d28b46d4b5403616cbdaf54d1db83b7a6788
黄翘楚 <2018011363@secoder.net>
2020-10-21 18:46:47 +0800

    使用enzyme进行前端测试 Ref #12

--- src/views/asset-management/asset-management.test.js
+++ src/views/asset-management/asset-management.test.js
@@ -1,21 +1,20 @@
 import React from 'react'
 import { Provider } from 'react-redux'
-import renderer from 'react-test-renderer'
+
 import AssetManagement from './index'
 import { store } from '@/utils/mockStore'
+import AssetInfo from './components/AssetInfo'
+import { configure, mount } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
 
-describe('AssetManagement test', () => {
-  let component
-  beforeEach(() => {
-    component = renderer.create(
-      <Provider store={store}>
-        <AssetManagement/>
-      </Provider>
-    )
-  })
-  afterEach(() => {
-    component.unmount()
-  })
-  it('render AssetManagement', () => {
+configure({ adapter: new Adapter() })
+
+describe('Enzyme Shallow', function () {
+  it('App\'s title should be Todos', function () {
+    const app = mount(<Provider store={store}><AssetManagement/></Provider>)
+
+    app.find('button').at(3).simulate('click')
+    app.find('button').at(4).simulate('click')
+    app.find(AssetInfo).find('button').at(1).simulate('click')
   })
 })
