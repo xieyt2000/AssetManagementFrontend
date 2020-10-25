630abc837c0ed9bac95a2be6068cf32db33aa4be
Xie Yuntong <2018011334@secoder.net>
2020-10-25 19:25:04 +0800

    reformat code. Ref #17

--- src/views/department-management/index.jsx
+++ src/views/department-management/index.jsx
@@ -30,100 +30,100 @@ const expandDepartment = (departments, tmpDepartmentList) => {
 }
 
 class DepartmentManagement extends React.Component {
-    state = {
-      expandedKeys: [],
-      searchValue: '',
-      autoExpandParent: true,
-      departments: [], // 有child的
-      departmentList: [] // 展开的
-    };
+  state = {
+    expandedKeys: [],
+    searchValue: '',
+    autoExpandParent: true,
+    departments: [], // 有child的
+    departmentList: [] // 展开的
+  }
 
-    onExpand = expandedKeys => {
-      this.setState({
-        expandedKeys,
-        autoExpandParent: false
+  onExpand = expandedKeys => {
+    this.setState({
+      expandedKeys,
+      autoExpandParent: false
+    })
+  }
+
+  onChange = e => {
+    const { value } = e.target
+    const { departments, departmentList } = this.state
+    const expandedKeys = departmentList
+      .map(item => {
+        if (item.name.indexOf(value) > -1) {
+          return getParentKey(item.id, departments)
+        }
+        return null
       })
-    };
+      .filter((item, i, self) => item && self.indexOf(item) === i)
+    this.setState({
+      expandedKeys,
+      searchValue: value,
+      autoExpandParent: true
+    })
+    console.log(expandedKeys)
+  }
 
-    onChange = e => {
-      const { value } = e.target
-      const { departments, departmentList } = this.state
-      const expandedKeys = departmentList
-        .map(item => {
-          if (item.name.indexOf(value) > -1) {
-            return getParentKey(item.id, departments)
-          }
-          return null
-        })
-        .filter((item, i, self) => item && self.indexOf(item) === i)
+  componentDidMount () {
+    this.getDepartment()
+  }
+
+  getDepartment = async () => {
+    const res = await departmentList()
+    const { data: departments, code } = res.data
+    if (code === 200) {
       this.setState({
-        expandedKeys,
-        searchValue: value,
-        autoExpandParent: true
+        departments: [departments]
       })
-      console.log(expandedKeys)
-    };
-
-    componentDidMount () {
-      this.getDepartment()
+      const tmpDepartmentList = []
+      expandDepartment([departments], tmpDepartmentList)
+      this.setState({
+        departmentList: tmpDepartmentList
+      })
     }
+  }
 
-    getDepartment = async () => {
-      const res = await departmentList()
-      const { data: departments, code } = res.data
-      if (code === 200) {
-        this.setState({
-          departments: [departments]
-        })
-        const tmpDepartmentList = []
-        expandDepartment([departments], tmpDepartmentList)
-        this.setState({
-          departmentList: tmpDepartmentList
-        })
-      }
-    }
-
-    render () {
-      const { searchValue, expandedKeys, autoExpandParent, departments } = this.state
-      const loop = data =>
-        data.map(item => {
-          const index = item.name.indexOf(searchValue)
-          const beforeStr = item.name.substr(0, index)
-          const afterStr = item.name.substr(index + searchValue.length)
-          const title =
-                    index > -1 ? (
-                      <span>
-                        {beforeStr}
-                        <span style={{ color: '#f50' }}>{searchValue}</span>
-                        {afterStr}
-                      </span>
-                    ) : (
-                      <span>{item.name}</span>
-                    )
-          if (item.children) {
-            return (
-              <TreeNode key={item.id} title={title}>
-                {loop(item.children)}
-              </TreeNode>
-            )
-          }
-          return <TreeNode key={item.id} title={title} />
-        })
-      return (
-        <div className='app-container'>
-          <div>
-            <Search style={{ marginBottom: 8 }} placeholder="Search" onChange={this.onChange} />
-            <Tree
-              onExpand={this.onExpand}
-              expandedKeys={expandedKeys}
-              autoExpandParent={autoExpandParent}
-            >
-              {loop(departments)}
-            </Tree>
-          </div>
+  render () {
+    const { searchValue, expandedKeys, autoExpandParent, departments } = this.state
+    const loop = data =>
+      data.map(item => {
+        const index = item.name.indexOf(searchValue)
+        const beforeStr = item.name.substr(0, index)
+        const afterStr = item.name.substr(index + searchValue.length)
+        const title =
+          index > -1 ? (
+            <span>
+              {beforeStr}
+              <span style={{ color: '#f50' }}>{searchValue}</span>
+              {afterStr}
+            </span>
+          ) : (
+            <span>{item.name}</span>
+          )
+        if (item.children) {
+          return (
+            <TreeNode key={item.id} title={title}>
+              {loop(item.children)}
+            </TreeNode>
+          )
+        }
+        return <TreeNode key={item.id} title={title}/>
+      })
+    return (
+      <div className='app-container'>
+        <div>
+          <Search style={{ marginBottom: 8 }} placeholder="Search" onChange={this.onChange}/>
+          <Tree
+            onExpand={this.onExpand}
+            expandedKeys={expandedKeys}
+            autoExpandParent={autoExpandParent}
+          >
+            {loop(departments)}
+          </Tree>
         </div>
-      )
-    }
+      </div>
+    )
+  }
 }
 
 export default DepartmentManagement
