c15998b6c1d9201ffcb5446be1fad02a541dcb61
Xie Yuntong <2018011334@secoder.net>
2020-10-25 21:18:48 +0800

    department change form. Ref #17

---
+++ src/views/department-management/change-department-form.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import { Button, Form, Input, Modal } from 'antd'
+import { PropTypes } from 'prop-types'
+
+class ChangeDepartmentForm extends Component {
+  render () {
+    const { visible, onCancel, onAdd, onDelete,
+      onEdit, form, confirmLoading, department } = this.props
+    return (
+      <Modal title="修改密码" visible={visible} confirmLoading={confirmLoading}
+        footer={[
+          <Button key='cancel' onClick={onCancel}>取消</Button>,
+          <Button key='delete' type='danger' onClick={onDelete}>删除部门</Button>,
+          <Button key='edit' type='primary' onClick={onEdit}>编辑部门</Button>,
+          <Button key='add' type='primary' onClick={onAdd}>添加新部门</Button>]
+        }
+      >
+        <Form>
+          <Form.Item label={'部门名称'}
+            extra={'编辑操作将当前部门名称修改为该名称，' +
+            '添加操作将为当前部门添加一个名为该名称的附属部门，删除操作将忽略此项（直接删除当前部门）'}>
+            {form.getFieldDecorator('name', {
+              rules: [{ required: true, message: '请输入部门名称' }],
+              initialValue: department.name
+            })(<Input placeholder='部门名称'/>)}
+          </Form.Item>
+        </Form>
+      </Modal>
+    )
+  }
+}
+
+ChangeDepartmentForm.propTypes = {
+  visible: PropTypes.bool,
+  onCancel: PropTypes.func,
+  onAdd: PropTypes.func,
+  onEdit: PropTypes.func,
+  onDelete: PropTypes.func,
+  form: PropTypes.object,
+  confirmLoading: PropTypes.bool,
+  department: PropTypes.object
+}
+
+export default Form.create()(ChangeDepartmentForm)
