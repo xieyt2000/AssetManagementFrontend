57d33a0549f5ed8e61aa98eaeeb9b71ee966c7d6
Xie Yuntong <2018011334@secoder.net>
2020-10-28 19:55:38 +0800

    beautify asset info. Ref #15

--- src/views/asset-management/components/AssetInfo.jsx
+++ src/views/asset-management/components/AssetInfo.jsx
@@ -5,7 +5,7 @@ import { PropTypes } from 'prop-types'
 class AssetInfo extends React.Component {
   render () {
     const { visible, onExit, confirmLoading, rowData } = this.props
-    const { type_name: isQuantity, quantity, value, name, history, description, parent, children,
+    const { type_name: isQuantity, quantity, value, name, description, parent, children,
       owner, department, status, start_time: startTime, prop, service_life: serviceLife } = rowData
     return (
       <div>
@@ -17,28 +17,22 @@ class AssetInfo extends React.Component {
                             返回
             </Button>]}
         >
-          <Descriptions>
-            <Descriptions.Item label='名称'>{name}</Descriptions.Item>
-            <br/><br/>
-            <Descriptions.Item label='描述'>{description}</Descriptions.Item>
-            <br/><br/>
-            <Descriptions.Item label='挂账人'>{owner}</Descriptions.Item>
-            <br/><br/>
-            <Descriptions.Item label='部门'>{department}</Descriptions.Item>
-            <br/><br/>
-            <Descriptions.Item label='状态'>{status}</Descriptions.Item>
-            <br/><br/>
-            <Descriptions.Item label='类型'>{isQuantity === 'AMOUNT' ? '数量型' : '条目型'}
+          <Descriptions column={3}>
+            <Descriptions.Item label='名称' span={3}>{name}</Descriptions.Item>
+            <Descriptions.Item label='描述' span={3}>{description}</Descriptions.Item>
+            <Descriptions.Item label='挂账人' span={3}>{owner}</Descriptions.Item>
+            <Descriptions.Item label='部门' span={3}>{department}</Descriptions.Item>
+            <Descriptions.Item label='状态' span={3}>{status}</Descriptions.Item>
+            <Descriptions.Item label='录入时间' span={3}>{startTime}</Descriptions.Item>
+            <Descriptions.Item label='类型'>
+              {isQuantity === 'AMOUNT' ? '数量型' : '条目型'}
             </Descriptions.Item>
             <Descriptions.Item label='数量'>{quantity}</Descriptions.Item>
             <Descriptions.Item label='价值'>{value}</Descriptions.Item>
-            <Descriptions.Item label='历史'>{history}</Descriptions.Item>
             <Descriptions.Item label='所属'>{parent}</Descriptions.Item>
             <Descriptions.Item label='包含'>{children}</Descriptions.Item>
-            <Descriptions.Item label='录入时间' >{startTime}</Descriptions.Item>
             <Descriptions.Item label='使用年限' >{serviceLife + '年'}</Descriptions.Item>
-            <br/><br/>
-            <Descriptions.Item label='自定义属性' >{prop}</Descriptions.Item>
+            <Descriptions.Item label='自定义属性' span={3}>{prop}</Descriptions.Item>
           </Descriptions>
         </Modal>
       </div>
