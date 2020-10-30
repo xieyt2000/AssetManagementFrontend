63551a9e237f07608f1c7ba6eab3fb1342e681a5
Xie Yuntong <2018011334@secoder.net>
2020-10-30 21:36:46 +0800

    改变父资产项描述。 Ref #24

--- src/views/asset-management/components/sharedFormItem.jsx
+++ src/views/asset-management/components/sharedFormItem.jsx
@@ -2,7 +2,7 @@ import { Form, Input } from 'antd'
 import React from 'react'
 
 export const parent = (form) => {
-  return (<Form.Item label={'父资产id'} help='填入父资产的id（在详情界面可以看到），如果没有父资产则不填'>
+  return (<Form.Item label={'父资产id'} help='填入父资产的id，如果没有父资产则不填'>
     {form.getFieldDecorator('parent_id', {})(<Input placeholder="父资产"/>)}
   </Form.Item>)
 }
