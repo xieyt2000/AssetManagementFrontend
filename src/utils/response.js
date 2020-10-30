ae531a7858bb9d69e552a99643aad507a1ea4e7c
Xie Yuntong <2018011334@secoder.net>
2020-10-30 20:12:42 +0800

    引入 handleResponse 统一处理请求响应逻辑。 Ref #22

--- src/utils/response.js
+++ src/utils/response.js
@@ -1,16 +1,29 @@
 import { message } from 'antd'
 
-export const handleResponse = (promise, oprMessage, stateToSet, self) => {
+export const handleResponse = (promise, oprMessage, self,
+  dataState = null, modalState = null, successFunc = null, finallyFunc = null) => {
   promise.then((res) => {
     if (res.data.code === 200) {
       message.success(oprMessage + '成功！')
-      self.setState({
-        [stateToSet]: res.data.data
-      })
+      if (dataState != null) {
+        self.setState({
+          [dataState]: res.data.data
+        })
+      }
+      if (successFunc != null) {
+        successFunc()
+      }
+      if (modalState != null) {
+        self.setState(modalState)
+      }
     } else {
       message.error(oprMessage + '失败，' + res.data.message)
     }
   }).catch(() => {
     message.error(oprMessage + '失败，请检查网络连接后重试！')
+  }).finally(() => {
+    if (finallyFunc != null) {
+      finallyFunc()
+    }
   })
 }
