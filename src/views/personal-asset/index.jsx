d0c86781a8fa263f7a059bb11aaabc9d1e18e147
yueyang2000 <2018011359@secoder.net>
2020-10-31 17:17:00 +0800

    merge query branch, preceed personal-asset. Ref #26

--- src/views/personal-asset/index.jsx
+++ src/views/personal-asset/index.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import { connect } from 'react-redux'
-
+import HelpCard from '../../components/HelpCard'
 class PersonalAsset extends React.Component {
   // constructor (props) {
   //   super(props)
   // }
 
   render () {
-    return (<div className='app-container'> <div>个人资产</div></div>)
+    const description = '查看个人名下的资产，并进行有关操作'
+    return (
+      <div className='app-container'>
+        <HelpCard title='个人资产' source={description} />
+      </div>
+    )
   }
 }
 
