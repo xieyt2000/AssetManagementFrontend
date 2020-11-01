6c115a2c1d22a0d981d75c9bdb2fc078471e26dd
Xie Yuntong <2018011334@secoder.net>
2020-11-01 15:18:19 +0800

    优化我的申请UI。 Ref #30

--- src/views/issue-personal/index.jsx
+++ src/views/issue-personal/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import HelpCard from '../../components/HelpCard'
-import { Table } from 'antd'
+import { Card, Table } from 'antd'
 import { renderAssignee, renderIssueType, renderIssueStatus } from '../../utils/issue'
 import { personalIssue } from '../../api/issue'
 import { getList } from '../../utils/list'
+
 const Column = Table.Column
 
 class IssuePersonal extends React.Component {
@@ -31,18 +32,21 @@ class IssuePersonal extends React.Component {
     const description = '作为企业员工，你可以在这里看到你提出的所有事项申请'
     return (
       <div className='app-container'>
-        <HelpCard title='个人事项' source={description} />
-        <Table
-          bordered rowKey="name"
-          dataSource={issueList}
-          expandIconColumnIndex={-1}
-          pagination={false}>
-          <Column title="事项id" dataIndex="nid" key="nid" align="center"/>
-          <Column title="涉及资产" dataIndex="asset" key="asset" align="center"/>
-          <Column title="事件类型" key="type_name" align="center" render={renderIssueType}/>
-          <Column title="接受人" key="initiator" align="center" render={renderAssignee}/>
-          <Column title="状态" key="status" align="center" render={renderIssueStatus}/>
-        </Table>
+        <HelpCard title='我的申请' source={description}/>
+        <br/>
+        <Card>
+          <Table
+            bordered rowKey="name"
+            dataSource={issueList}
+            expandIconColumnIndex={-1}
+            pagination={false}>
+            <Column title="事项id" dataIndex="nid" key="nid" align="center"/>
+            <Column title="涉及资产" dataIndex="asset" key="asset" align="center"/>
+            <Column title="事件类型" key="type_name" align="center" render={renderIssueType}/>
+            <Column title="接受人" key="initiator" align="center" render={renderAssignee}/>
+            <Column title="状态" key="status" align="center" render={renderIssueStatus}/>
+          </Table>
+        </Card>
       </div>
     )
   }
