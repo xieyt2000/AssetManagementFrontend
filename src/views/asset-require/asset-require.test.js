87aea4d1dcf3669c9933953281f36755d39e08e4
黄翘楚 <2018011363@secoder.net>
2020-11-01 20:11:08 +0800

    完成asset-require单元测试Ref #27

---
+++ src/views/asset-require/asset-require.test.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Provider } from 'react-redux'
+import AssetRequire from './index'
+import { store } from '@/utils/mockStore'
+
+import { configure, mount } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import '@/mock'
+configure({ adapter: new Adapter() })
+
+describe('asset-require', function () {
+  let app
+  beforeEach(() => {
+    app = mount(
+      <Provider store={store}>
+        <AssetRequire/>
+      </Provider>
+    )
+    app.find('AssetRequire').setState({
+      assetList: [{}]
+    })
+  })
+  it('test cancel', function () {
+    app.find('button').simulate('click')
+    app.find('Modal').find('button').at(1).simulate('click')
+  })
+  it('test ok', function () {
+    app.find('button').simulate('click')
+    app.find('Modal').find('button').at(2).simulate('click')
+  })
+})
