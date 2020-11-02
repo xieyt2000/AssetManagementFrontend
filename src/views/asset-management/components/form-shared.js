a050fb1491ab5dbd8c794c5effd5e0e17a015f21
Xie Yuntong <2018011334@secoder.net>
2020-11-02 20:05:21 +0800

    自定义属性表单的生成与提取对象。 Ref #28

--- src/views/asset-management/components/form-shared.js
+++ src/views/asset-management/components/form-shared.js
@@ -1,4 +1,7 @@
 import STATUS from '../../../utils/asset'
+import { Form, Input } from 'antd'
+import React from 'react'
+
 export function getStatusArr () {
   const statusArr = []
   for (const key in STATUS) {
@@ -6,7 +9,29 @@ export function getStatusArr () {
   }
   return statusArr
 }
+
 export const formLayout = {
   labelCol: { sm: { span: 4 } },
   wrapperCol: { sm: { span: 16 } }
+}
+
+export function getCustomPropFormItem (customPropList, form) {
+  return (customPropList.map((propName) => (
+    <Form.Item label={propName} key={propName}>
+      {form.getFieldDecorator(`${propName}`)(
+        <Input placeholder={propName}/>
+      )}
+    </Form.Item>
+  )))
+}
+
+export function putCustom (form, customPropList) {
+  const newForm = form
+  newForm['custom'] = {}
+  Object.keys(form).forEach((key) => {
+    if (customPropList.indexOf(key) > -1) {
+      newForm['custom'][key] = form[key]
+    }
+  })
+  return newForm
 }
